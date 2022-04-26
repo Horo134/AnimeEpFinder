3345c2ac814a83e4863997e831d15df94b23c12a
Sergey Romanov <study-front@mail.ru>
2022-04-27 00:31:39 +0300

    test v.6

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.engine("hbs", expressHbs.engine(
 // Сообщаем express, что в качестве шаблонизатора используется "hbs".
 app.set('view engine', 'hbs');
 // Сообщаем express, что шаблона шаблонизаторая (вью) находятся в папке "ПапкаПроекта/views".
-hbs.registerPartials(`${__dirname}/views`);
+hbs.registerPartials(`${__dirname}/views/entries`);
 // Подключаем middleware morgan с режимом логирования "dev", чтобы для каждого HTTP-запроса на сервер в консоль выводилась информация об этом запросе.
 app.use(logger('dev'));
 // Подключаем middleware, которое сообщает epxress, что в папке "ПапкаПроекта/public" будут находится статические файлы, т.е. файлы доступные для скачивания из других приложений.
