5769978f45998acd18ce2956061171e519b81da4
Sergey Romanov <study-front@mail.ru>
2022-04-26 23:55:31 +0300

    test v.2

--- app.js
+++ app.js
@@ -20,12 +20,11 @@ const app = express();
 // Сообщаем express, что в качестве шаблонизатора используется "hbs".
 app.set('view engine', 'hbs');
 // Сообщаем express, что шаблона шаблонизаторая (вью) находятся в папке "ПапкаПроекта/views".
-hbs.registerPartials(`${__dirname}/views`);
+hbs.registerPartials(__dirname+'/views/entries');
 // Подключаем middleware morgan с режимом логирования "dev", чтобы для каждого HTTP-запроса на сервер в консоль выводилась информация об этом запросе.
 app.use(logger('dev'));
 // Подключаем middleware, которое сообщает epxress, что в папке "ПапкаПроекта/public" будут находится статические файлы, т.е. файлы доступные для скачивания из других приложений.
-// app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(process.env.PWD, `${__dirname}/views`)));
+app.use(express.static(path.join(__dirname, 'public')));
 // Подключаем middleware, которое позволяет читать содержимое body из HTTP-запросов типа POST, PUT и DELETE.
 app.use(express.urlencoded({ extended: true }));
 // Подключаем middleware, которое позволяет читать переменные JavaScript, сохранённые в формате JSON в body HTTP-запроса.
